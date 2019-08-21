7ce6068bfa7989102461e16dcc5b344ee9fcb255
KahLokKee <e0308990@u.nus.edu>
2019-08-21 14:03:33 +0800

    Implemented functionalities for Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,26 +2,66 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    public static void main(String[] args) {
-        ArrayList<String> tasks = new ArrayList<>();
+    private ArrayList<Task> tasks = new ArrayList<>();
+
+    public Duke() {
+        greet();
+    }
+
+    public void greet() {
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+    }
+
+    public void exit() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public void getTask(int index) {
+        Task task = tasks.get(index - 1);
+        String statusIcon = task.getStatusIcon();
+        String description = task.getDescription();
+        System.out.println("[" + statusIcon + "] " + description);
+    }
+
+    public void listTask() {
+        int index = 1;
+        for (Task task : tasks) {
+            System.out.print(index + ".");
+            getTask(index);
+            index++;
+        }
+    }
+
+    public void setTaskDone(int index) {
+        Task task = tasks.get(index - 1);
+        task.markAsDone();
+        System.out.println("Nice! I've marked this task as done:");
+        getTask(index);
+    }
+
+    public void addTask(String description) {
+        Task task = new Task(description);
+        tasks.add(task);
+        System.out.println("added: " + task.getDescription());
+    }
+
+    public static void main(String[] args) {
+        Duke duke = new Duke();
         Scanner sc = new Scanner(System.in);
+
         while (true) {
-            if (sc.hasNextLine()) {
-                String command = sc.nextLine();
-                if (command.equals("bye")) {
-                    System.out.println("Bye. Hope to see you again soon!");
-                    break;
-                } else if (command.equals("list")) {
-                    int count = 1;
-                    for (String item : tasks) {
-                        System.out.println(count + ". " + item);
-                        count++;
-                    }
-                } else {
-                    tasks.add(command);
-                    System.out.println("added: " + command);
-                }
+            String command = sc.nextLine();
+            String[] splitCommand = command.split(" ");
+            if (command.equals("bye")) {
+                duke.exit();
+                return;
+            } else if (command.equals("list")) {
+                duke.listTask();
+            } else if (splitCommand[0].equals("done")) {
+                int index = Integer.parseInt(splitCommand[1]);
+                duke.setTaskDone(index);
+            } else {
+                duke.addTask(command);
             }
         }
     }
